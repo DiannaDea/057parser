69e990be588fb0fe6b26231bd5fc8666e31e3cf6
DiannaDea <dianababurina74@gmail.com>
2018-01-05 16:54:53 +0200

    added function for image parse

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,71 @@
-var express = require('express');
-var router = express.Router();
-var parser = require('rss-parser');
-var request = require("request");
-var cheerio = require("cheerio");
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+const parser = require('rss-parser');
+const request = require("request");
+const cheerio = require("cheerio");
 
+const URL_RSS = 'https://www.057.ua/rss';
 
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+router.get("/news/:count", (req, res) => {
+    getLinks(req.params.count, URL_RSS)
+        .then(links => {
+            Promise.all(links.map(link => parseHTML(link)))
+                .then(feed => {
+                    res.render('index', {feed: feed});
+                })
+        });
 });
 
-router.get("/feed/:count", (req, res) => {
-
-    getLinks(req.params.count).then(links => {
-        Promise.all(links.map(link => parseHTML(link))).then(feed => {
-            console.log(feed);
-            res.render('index', {feed: feed});
-        })
-    });
-});
-
-function getLinks(count) {
-
+function getLinks(count, url_rss) {
     return new Promise(function (resolve, reject) {
         const links = [];
-        parser.parseURL('https://www.057.ua/rss', function (err, parsed) {
-            const slice_parsed = parsed.feed.entries.slice(0, count);
-            slice_parsed.map(item => {
-                links.push(item.link);
-            });
-            resolve(links);
+        parser.parseURL(url_rss, function (err, parsed) {
+            if (!err) {
+                const slice_parsed = parsed.feed.entries.slice(0, count);
+                slice_parsed.map(item => {
+                    links.push(item.link);
+                });
+                resolve(links);
+            }
+            else {
+                reject(err);
+            }
         });
     })
 }
-
-
 
 function parseHTML(link) {
     return new Promise(function (resolve, reject) {
         request(link, function (error, response, body) {
             if (!error) {
-                let $ = cheerio.load(body);
-                let title = $(".title-container h1").text();
-                let text = $(".article-text p").text();
-                let image_src = $(".article-photo--main img").attr("src");
-                let new_obj = ({title: title, text: text, link: link});
-                resolve(new_obj);
+                let feed_item = getFeedFromHTML(body, link);
+                resolve(feed_item);
             } else {
+                reject(error);
                 console.log("Произошла ошибка: " + error);
             }
         });
     })
 }
+
+function getFeedFromHTML(body, link) {
+    let $ = cheerio.load(body);
+    let title = $(".title-container h1").text();
+    let text = $(".article-text p:not(:last-child)").text();
+    let img_src_arr = getImages($);
+    return ({title: title, text: text, link: link, img: img_src_arr})
+}
+
+function getImages($) {
+    let img_src_arr = [];
+    let article_photo = $(".article-photo img").attr("src");
+    if (article_photo !== undefined) img_src_arr.push(article_photo);
+    $(".fotorama img").each(function () {
+        img_src_arr.push($(this).attr("src"));
+    });
+    return img_src_arr;
+}
+
 
 module.exports = router;
